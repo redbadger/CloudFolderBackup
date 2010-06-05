dafbf12daa7488eca53ff36f3dfa0070d69ad7bc
David Wynne <d_wynne@yahoo.com>
2010-06-05 14:03:26 +0100

    Added a little console feedback.

--- RedBadger.CloudFolderBackup.ConsoleApp/Program.cs
+++ RedBadger.CloudFolderBackup.ConsoleApp/Program.cs
@@ -9,12 +9,16 @@
             try
             {
                 Agent.Run();
+                Console.WriteLine("Success!");
             }
             catch (Exception exception)
             {
+                Console.WriteLine("Failed:");
                 Console.WriteLine(exception);
             }
 
+            Console.WriteLine();
+            Console.WriteLine(string.Format("Finished: {0}", DateTime.Now));
             Console.ReadLine();
         }
     }
