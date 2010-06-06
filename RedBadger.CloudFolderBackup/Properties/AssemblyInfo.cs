24774a9532a478784d73905c4c96831814ed8bff
David Wynne <d_wynne@yahoo.com>
2010-06-06 10:47:11 +0100

    Updating AssemblyInfo.cs

--- RedBadger.CloudFolderBackup/Properties/AssemblyInfo.cs
+++ RedBadger.CloudFolderBackup/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RedBadger.CloudFolderBackup")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Über simple app that takes a folder, zips it and uploads it to Rackspace Cloud Files.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Red Badger Consulting Limited")]
 [assembly: AssemblyProduct("RedBadger.CloudFolderBackup")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCopyright("Copyright © Red Badger Consulting Limited 2010. All Rights Reserved")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
